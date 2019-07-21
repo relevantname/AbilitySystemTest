b744213e4a8b7e07682112221a28435a19aa9103
Onur <onur1992@gmail.com>
2019-07-21 23:11:37 +0300

    Adding Ability System Scripts

---
+++ Assets/Objects/MagicBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBall : ShootableObject
+{
+    public GameObject blowEffect;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        IDamagable damagable = collision.collider.GetComponent<IDamagable>();
+        damagable?.GetDamage(heal_damageAmount);
+
+        if(blowEffect != null)Instantiate(blowEffect, collision.transform.position, Quaternion.identity);
+    }
+}
